ce6fafe3aff12c80cf615c9a9d5498b7cfb18bf0
ilyanikoniko <iliya05163@gmail.com>
2025-03-25 00:54:41 +0300

    функция подсчёта частот встречающихся символов и функция извлечения ключа

--- lab_1/task2/task2.py
+++ lab_1/task2/task2.py
@@ -23,3 +23,46 @@ def write_to_file(filename, data):
             file.write(data)
     except Exception as e:
         print(f"Ошибка при записи в файл {filename}: {e}")
+
+
+def calculate_char_frequency(text):
+    """
+    Вычисляет частоту встреч символов в тексте
+    :param text: исходный текст
+    :return: словарь, где ключ/значение - символ/частота его встречаемости
+    """
+    try:
+        dictionary_freq = {}
+        text_len = len(text)
+
+        for char in text:
+            dictionary_freq[char] = dictionary_freq.get(char, 0) + 1
+
+        return {char: (count / text_len) * 100 for char, count in dictionary_freq.items()}
+    except Exception as e:
+        print(f"Ошибка при вычислении процента символов: {e}")
+        return {}
+
+
+def create_crypt_key(supportive_dict, replace_dict):
+    """
+    Создает словарь encryption_key на основе вспомогательного и основного словарей
+    :param supportive_dict: вспомогательный словарь замен SUPPORTIVE_DICT
+    :param replace_dict: основной словарь замен REPLACE_DICT
+    :return: ключ-словарь  encryption_key
+    """
+    try:
+        reverse_supportive_dict = {val: key for key, val in supportive_dict.items()}
+
+        encryption_key = {}
+        for key, val in replace_dict.items():
+            if key in reverse_supportive_dict:
+                original_key = reverse_supportive_dict[key]
+                encryption_key[original_key] = val
+        for key, val in replace_dict.items():
+            if key not in reverse_supportive_dict:
+                encryption_key[key] = val
+        return encryption_key
+    except Exception as e:
+        print(f"Ошибка при создании ключа шифрования: {e}")
+        return encryption_key
