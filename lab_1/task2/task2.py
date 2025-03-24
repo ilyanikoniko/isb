fafb679dadd9d27c95114177503d1db903925194
ilyanikoniko <iliya05163@gmail.com>
2025-03-25 01:07:39 +0300

    финальная версия

--- lab_1/task2/task2.py
+++ lab_1/task2/task2.py
@@ -1,3 +1,6 @@
+from variables import SUPPORTIVE_DICT, REPLACE_DICT, ENCRYPTED_TEXT, DECRYPTED_TEXT, KEY
+
+
 def replace_chars_safely(text, replacements):
     """
     Заменяет символы в тексте по словарю replacements
@@ -66,3 +69,47 @@ def create_crypt_key(supportive_dict, replace_dict):
     except Exception as e:
         print(f"Ошибка при создании ключа шифрования: {e}")
         return encryption_key
+
+
+def main():
+    try:
+        with open(ENCRYPTED_TEXT, 'r', encoding='utf-8') as file:
+            text = file.read()
+
+        print("\nЗашифрованный текст:\n")
+        print(text)
+
+        text = replace_chars_safely(text, SUPPORTIVE_DICT)
+
+        print("\nПрименение вспомогательных замен:\n")
+        print(text)
+
+        freq_dict = calculate_char_frequency(text)
+
+        print("\nЧастота встречающих символов в зашифрованном тексте:\n")
+        print(dict(sorted(freq_dict.items(), key=lambda item: item[1], reverse=True)))
+
+        text = replace_chars_safely(text, REPLACE_DICT)
+
+        print("\nДешифрованный текст:\n")
+        print(text)
+        print("\n")
+        write_to_file(DECRYPTED_TEXT, text)
+
+        encryption_key = create_crypt_key(SUPPORTIVE_DICT, REPLACE_DICT)
+        if encryption_key:
+            print(encryption_key)
+
+        write_to_file(DECRYPTED_TEXT, text)
+        write_to_file(KEY, str(encryption_key))
+        print("\nДешифрованный текст успешно записан в 'decrypted_text.txt'.")
+        print("\nКлюч успешно записан в 'key.txt'.")
+
+    except FileNotFoundError:
+        print("Ошибка: Файл 'encrypted_text.txt' не найден.")
+    except Exception as e:
+        print(f"Неизвестная ошибка: {e}")
+
+
+if __name__ == "__main__":
+    main()
