dde5093b5e6e6bd0703a49c64f9d69b24cfe0cb5
ilyanikoniko <iliya05163@gmail.com>
2025-04-15 21:39:27 +0300

    третий тест

--- lab_2/nist_tests.py
+++ lab_2/nist_tests.py
@@ -47,3 +47,49 @@ def runs_test(binary_sequence):
     p_value = math.erfc(numerator / denominator)
 
     return p_value
+
+
+def longest_run_test(binary_sequence, block_size=8):
+    """
+    Тест на самую длинную последовательность единиц в блоке
+    :param binary_sequence: Последовательность(строка) из "0" и "1"
+    :param block_size: Размер блока
+    :return: P-значение последовательности
+    """
+
+    n = len(binary_sequence)
+    if n % block_size != 0:
+        raise ValueError(f"Длина последовательности ({n}) должна быть кратна размеру блока ({block_size})")
+
+    num_blocks = n // block_size
+
+    pi = [0.2148, 0.3672, 0.2305, 0.1875]
+
+    v = [0, 0, 0, 0]  # v0, v1, v2, v3
+
+    for i in range(num_blocks):
+        block = binary_sequence[i * block_size: (i + 1) * block_size]
+        max_run = 0
+        current_run = 0
+
+        for bit in block:
+            if bit == '1':
+                current_run += 1
+                max_run = max(max_run, current_run)
+            else:
+                current_run = 0
+
+        if max_run <= 1:
+            v[0] += 1
+        elif max_run == 2:
+            v[1] += 1
+        elif max_run == 3:
+            v[2] += 1
+        else:  # max_run >= 4
+            v[3] += 1
+
+    hi_square = sum((v[i] - num_blocks * pi[i]) ** 2 / (num_blocks * pi[i]) for i in range(4))
+
+    p_value = gammaincc(1.5, hi_square / 2)
+
+    return p_value
